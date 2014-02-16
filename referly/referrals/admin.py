1ff9c83200eafbca4e271898c0b92fa3d3aabcb9
Nar Kumar <neokya@gmail.com>
2014-02-17 00:36:40 +0530

    add referrals admin panel

--- referly/referrals/admin.py
+++ referly/referrals/admin.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Referral
+
+
+class ReferralAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_created', 'user',)
+    list_filter = ('date_created',)
+    search_fields = ('title',)
+
+admin.site.register(Referral, ReferralAdmin)
