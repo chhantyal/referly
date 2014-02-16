4917765d42f65a817d0fe8c36cc47009d85f7547
Nar Kumar <neokya@gmail.com>
2014-02-17 02:36:14 +0530

    add API urls

---
+++ referly/apiv1/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import patterns, include, url
+
+from .views import (UserRetriveAPIView, ReferralListCreateAPIView,
+                                    ReferralRetriveUpdateDestroyAPIView)
+
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+urlpatterns = patterns('',
+    # authentication
+    url(r'^token/$', obtain_auth_token),
+
+    # user API endpoint
+    url(r'^user/(?P<username>[\w-]+)$', UserRetriveAPIView.as_view(), name='api_user'),
+
+    # referrals API endpoint
+    url(r'^referrals/$', ReferralListCreateAPIView.as_view(), name='api_referrals_list'),
+
+    # referral detail API endpoint
+    url(r'^referral/(?P<slug>[\w-]+)/$', ReferralRetriveUpdateDestroyAPIView.as_view(),
+                                                name='api_referral_detail'),
+)
