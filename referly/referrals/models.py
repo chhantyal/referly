122ce7ab90587a6a9285ab0599d278bc9a771238
Nar Kumar <neokya@gmail.com>
2014-02-17 00:35:06 +0530

    add referrals models

--- referly/referrals/models.py
+++ referly/referrals/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+import uuid
+import datetime
 
-# Create your models here.
+from django.db import models, IntegrityError
+from django.utils.text import slugify
+
+
+class Referral(models.Model):
+    """
+    Main models for referrals app.
+    """
+    title = models.CharField(max_length=250)
+    slug = models.CharField(unique=True, blank=True, max_length=250)
+    user = models.ForeignKey('users.User', related_name='referrals')
+    date_created = models.DateTimeField(auto_now_add=True, default=datetime.datetime.now)
+    date_updated = models.DateTimeField(auto_now=True, default=datetime.datetime.now)
+    is_active = models.BooleanField(default=True)
+    clicks = models.PositiveIntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        """
+        When slug/referral_id is not given, slugfy title and save it as slug
+        """
+        if not self.slug:
+            # make it unique using uuid module
+            self.slug = slugify("{slug}-{uid}".format(slug=self.title, uid=str(uuid.uuid4())[:4]))
+        super(Referral, self).save(*args, **kwargs)
