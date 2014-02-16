10bea66ffc553c6189179216ba2c4295385c5c63
Nar Kumar <neokya@gmail.com>
2014-02-17 01:40:55 +0530

    add better fixtures data for testing

--- referly/users/admin.py
+++ referly/users/admin.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from django.contrib.auth.models import Group
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import User
