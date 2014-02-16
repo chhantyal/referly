5b9a051c5bdad33555e147b8915dad3a0b440734
Nar Kumar <neokya@gmail.com>
2014-02-17 01:28:34 +0530

    better user admin

--- referly/users/admin.py
+++ referly/users/admin.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import User
@@ -9,5 +10,8 @@ class UserAdmin(admin.ModelAdmin):
     create_form_class = UserCreationForm
     update_form_class = UserChangeForm
 
+    list_display = ('username', 'email', 'is_staff',)
+    list_filter = ('email',)
+    search_fields = ('username', 'email',)
 
 admin.site.register(User, UserAdmin)
