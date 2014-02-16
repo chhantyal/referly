256cf74af184ff85eafcc20fad0161f163990171
Nar Kumar <neokya@gmail.com>
2014-02-17 00:32:54 +0530

    create referrals app

--- referly/config/settings.py
+++ referly/config/settings.py
@@ -45,7 +45,7 @@ class Common(Configuration):
     # Apps specific for this project go here.
     LOCAL_APPS = (
         'users',  # custom users app
-        # Your stuff: custom apps go here
+        'referrals',
     )
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
