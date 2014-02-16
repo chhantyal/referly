de6af2dd508c4a1d6ab07202f3bedbc671319757
Nar Kumar <neokya@gmail.com>
2014-02-17 02:54:17 +0530

    add js function to send ajax request - to create referral

--- referly/static/js/referly.js
+++ referly/static/js/referly.js
@@ -1,1 +1,20 @@
-/* Project specific Javascript goes here. */
+// Custom JS
+
+// create referral via ajax request
+function createReferral(){
+    var dataString = $('form#referral_create').serialize();
+    console.log(dataString);
+    $.ajax({
+        type: "POST",
+        url: "/apiv1/referrals/",
+        data: dataString,
+        success: function(data) {
+            console.log(data);
+            var html ='<tr><td>'+ data.title +'</td><td>'+ data.slug +'</td><td>'+ data.clicks +'</td><td><a href="#">Edit</a></td><td><a href="#">Delete</a></td></tr>';
+            $('.table tbody').append(html);
+            var message_html = '<div class="alert alert-info"><a class="close" data-dismiss="alert">×</a><span>' + 'New referral added.' + '</span></div>'
+            $('#messages').append(message_html);
+        }
+     });
+     return false;
+}
